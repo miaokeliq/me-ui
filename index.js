f2173a8d18a9c0dc9519c9faf59dfa43bd7e30ad
miaokeliq <921418076@qq.com>
2023-02-16 15:29:37 +0800

    dist/index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// 默认的程序入口，要导入需要导出的所有东西
+
+import Button from "./src/button.vue";
+import ButtonGroup from "./src/button-group.vue";
+import Icon from "./src/icon.vue";
+
+export { ButtonGroup, Button, Icon };
+
+//
